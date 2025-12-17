05fa1de8c76df2adeab3734d66bd096acde3b02b
gadesathwika19 <gadesathwika19@gmail.com>
2025-12-17 23:00:54 +0000

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,8 @@
+INSERT INTO sales (order_date, region, category, sales, profit) VALUES
+('2017-11-18', 'West', 'Office Supplies', 731.94, 219.58),
+('2016-06-02', 'South', 'Furniture', 957.58, -383.03),
+('2015-06-14', 'West', 'Technology', 907.15, 68.36),
+('2015-06-15', 'West', 'Office Supplies', 18.50, 5.20),
+('2018-04-05', 'South', 'Office Supplies', 15.55, 5.44),
+('2018-04-15', 'Central', 'Technology', 1706.18, 85.31),
+('2018-04-15', 'Central', 'Furniture', 261.96, -41.91);
